ea499ba1c04781a5d075419274a3dd937a8d0bd7
verynicevanessa <126076156+verynicevanessa@users.noreply.github.com>
2024-03-10 09:59:25 +0100

    Me Button -> projects

--- src/components/Decision.vue
+++ src/components/Decision.vue
@@ -48,7 +48,7 @@ export default {
   },
   methods: {
     handleMe() {
-      this.$router.push("/");
+      this.$router.push("/projects");
     },
     handleYou() {
       this.decisionMade = true;

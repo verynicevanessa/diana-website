fee4f667ced6750fb3cd9bcb097d121fd12c692c
NastyaOleyna <nastyaolejnyk@gmail.com>
2024-03-13 21:19:40 +0200

    changed project detail

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,32 +1,36 @@
 <template>
-    <transition name="fade">
-      <div v-if="menuOpen" class="menu-overlay" :class="{ 'active': menuOpen }" ref="MenuOverlay">
+  <transition name="fade">
+    <div v-if="menuOpen" class="menu-overlay" :class="{ active: menuOpen }" ref="MenuOverlay">
+      <!-- Your menu overlay content goes here -->
+      <MenuOverlay @close-menu="closeMenu" />
+    </div>
+  </transition>
+  <a @click="toggleMenu" class="menu-button" aria-label="Toggle menu">
+    <img src="../assets/DW-Menu.svg" alt="SVG Image" />
+  </a>
 
-        <!-- Your menu overlay content goes here -->
-        <MenuOverlay @close-menu="closeMenu"/>
-      </div>
-    </transition>
   <div class="menu-wrapper">
     <div class="menu">
-      <span><router-link to="/projects" @click="closeMenu"><h1>DIANA WEISMAN</h1></router-link></span>
-      <router-link to="/" @click="closeMenu"><img src="/DWLogo.png" class="logo"></router-link>
-      <a @click="toggleMenu" aria-label="Toggle menu"><img src="../assets/DW-Menu.svg" class="menu-button" alt="SVG Image"></a>
+      <span>
+        <router-link to="/projects" @click="closeMenu">
+          <h1>DIANA WEISMAN</h1>
+        </router-link>
+      </span>
+      <router-link to="/" @click="closeMenu"><img src="/DWLogo.png" class="logo" /></router-link>
     </div>
   </div>
-  
-
 </template>
 
 <script>
-import MenuOverlay from './MenuOverlay.vue';
+import MenuOverlay from "./MenuOverlay.vue";
 
 export default {
   components: {
-    MenuOverlay
+    MenuOverlay,
   },
   data() {
     return {
-      menuOpen: false
+      menuOpen: false,
     };
   },
   watch: {
@@ -34,73 +38,75 @@ export default {
     menuOpen(newValue) {
       if (newValue) {
         // If menuOpen is true, disable scrolling
-        document.body.style.overflow = 'hidden';
+        document.body.style.overflow = "hidden";
       } else {
         // If menuOpen is false, enable scrolling
-        document.body.style.overflow = '';
+        document.body.style.overflow = "";
       }
-    }
+    },
   },
   methods: {
-
     toggleMenu() {
       this.menuOpen = !this.menuOpen;
-      console.log('Menu state:', this.menuOpen); 
+      console.log("Menu state:", this.menuOpen);
     },
     closeMenu() {
-    this.menuOpen = false; // Directly close the menu
-  }
-  }
+      this.menuOpen = false; // Directly close the menu
+    },
+  },
 };
 </script>
-
-
 
 <style>
 .menu-wrapper {
   top: 0;
   left: 0;
-  width: 100%; 
-  display: flex; 
-  justify-content: space-between; 
-  padding: 0 20px; 
-  box-sizing: border-box; 
-  z-index: 900; 
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+  box-sizing: border-box;
+  z-index: 900;
   position: relative;
   /* mix-blend-mode: difference; */
 }
 
 .menu {
   display: flex;
-  justify-content: center; /* Center the menu items */
-  width: 100%; /* Ensure the menu spans the full width of its container */
+  justify-content: center;
+  /* Center the menu items */
+  width: 100%;
+  /* Ensure the menu spans the full width of its container */
   position: relative;
   z-index: 900;
 }
 
+.menu span {
+  position: absolute;
+  left: 0;
+}
+
 .menu-button {
   width: 2em;
-  margin: 10px 0 0 0;
-  z-index: 900;
-  mix-blend-mode: exclusion;
+  z-index: 999;
+  position: fixed;
+  right: 1em;
+  top: 1em;
+  mix-blend-mode: difference;
 }
 
 .logo {
   width: 100%;
-  max-width: 200px; 
+  max-width: 200px;
   z-index: 900;
-/* 
+  /* 
   background-image: url(/2.png);
      filter: blur(3px);
      background-size: contain; */
 }
 
-.menu-wrapper .logo:hover  {
- mix-blend-mode: difference;
-}
-
-a {
-  margin-left: auto; /* Push the button to the right */
+.menu-wrapper .logo:hover {
+  mix-blend-mode: difference;
 }
 
 .menu-overlay {
@@ -109,14 +115,22 @@ a {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(255, 255, 255, 0.234); /* Adjust background color as needed */
-  z-index: 800; /* Ensure it's above other content */
-  transition: transform 0.3s ease; /* Smooth transition for sliding animation */
-  transform: translateY(-100%); /* Initially hide the menu overlay */
+  background-color: rgba(255,
+      255,
+      255,
+      0.234);
+  /* Adjust background color as needed */
+  z-index: 800;
+  /* Ensure it's above other content */
+  transition: transform 0.3s ease;
+  /* Smooth transition for sliding animation */
+  transform: translateY(-100%);
+  /* Initially hide the menu overlay */
 }
 
 .menu-overlay.active {
-  transform: translateY(0); /* Slide the menu overlay into view */
+  transform: translateY(0);
+  /* Slide the menu overlay into view */
 }
 
 .fade-enter-active,
@@ -128,6 +142,4 @@ a {
 .fade-leave-to {
   opacity: 0;
 }
-
 </style>
-
--- src/mixins/useProjectData.js
+++ src/mixins/useProjectData.js
@@ -3,6 +3,13 @@ export default {
   created() {
     this.$store.dispatch("loadProjects");
   },
+  methods: {
+    getProjectBySlug(slug) {
+      return this.projects.find((project) => {
+        return project.projectSlug === slug;
+      })
+    },
+  },
   computed: {
     projects() {
       return this.$store.state.loadedProjects;

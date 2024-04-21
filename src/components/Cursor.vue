e17d711d16ee529f3d99236ced2db716f7399b6c
NastyaOleyna <nastyaolejnyk@gmail.com>
2024-04-21 19:10:05 +0300

    centred cursor

--- src/components/Cursor.vue
+++ src/components/Cursor.vue
@@ -9,12 +9,13 @@ import SFblink from '/SF-blink.svg';
 
 export default {
   setup() {
+    const cursorHotspot = { x: 12, y: 12 };
     // This function will be called when the component is mounted to the DOM
     const handleGlobalClick = () => {
       console.log("Global click event occurred"); // Log when the global click event occurs
-      document.body.style.cursor = `url(${SFblink}), auto`;
+      document.body.style.cursor = `url(${SFblink}) ${cursorHotspot.x} ${cursorHotspot.y}, auto`;
       setTimeout(() => {
-        document.body.style.cursor = `url(${SFeye}), auto`;
+        document.body.style.cursor = `url(${SFeye}) ${cursorHotspot.x} ${cursorHotspot.y}, auto`;
         console.log("Cursor changed back to SF-eye.svg"); // Log when cursor changes back
       }, 150);
     };
@@ -39,7 +40,7 @@ export default {
 <style>
 
 body , a {
-  cursor: url('../assets/SF-eye.svg'), auto;
+  cursor: url('../assets/SF-eye.svg') 12 12, auto;
 }
 
 

0d26c1a8d72005b5eb78266f70fbe32ab0ddcee3
NastyaOleyna <nastyaolejnyk@gmail.com>
2024-05-06 05:25:17 +0300

    fixed masonry wall

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -2,19 +2,39 @@
 import Footer from "../components/Footer.vue";
 import useProjectData from "@/mixins/useProjectData";
 import Project from "@/components/Project.vue";
-
+import MasonryWall from "@yeger/vue-masonry-wall";
 
 export default {
   mixins: [useProjectData],
   components: { Footer, Project },
-}
+  data() {
+    return {
+      masonryKey: 0, // ключ для пересоздания компонента Masonry
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.masonryKey++; // Изменение ключа вызовет пересоздание компонента
+    }, 100); // задержка 500 мс
+  },
+};
 </script>
 
 <template>
-  <masonry-wall v-if="sortedByPriotity.length" :items="this.sortedByPriotity" :max-columns="3" :column-width="270" :gap="16" style="margin-top: 15vh;">
+  <MasonryWall
+    v-if="sortedByPriotity.length"
+    :items="this.sortedByPriotity"
+    :max-columns="3"
+    :column-width="200"
+    :gap="16"
+    :key="masonryKey"
+    style="margin-top: 10vh"
+  >
     <template #default="{ item, index }">
-      <Project :project="item"/>
+      <Project :project="item" />
     </template>
-  </masonry-wall>
+  </MasonryWall>
   <Footer></Footer>
 </template>
+
+<style></style>

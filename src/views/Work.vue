c00e0a60c9de73bf4d879f37eccd7e52f764a002
verynicevanessa <vanessa@verynice.studio>
2024-06-15 17:14:43 +0200

    Loading Transition

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -9,35 +9,98 @@ export default {
   components: { Footer, Project },
   data() {
     return {
-      masonryKey: 0, 
+      masonryKey: 0,
+      showContent: false,
+      isLoading: true
     };
   },
+  mounted() {
+    this.loadProjects();
+  },
   methods: {
+    loadProjects() {
+      // Simulating a load delay for demonstration purposes.
+      setTimeout(() => {
+        this.showContent = true;
+        this.isLoading = false;
+      }, 1000); // Adjust the timeout duration as needed.
+    },
     onLayout() {
       this.$nextTick(() => {
         this.masonryKey++; // Trigger a re-render
       });
+    },
+    handleBeforeEnter(el) {
+      el.style.opacity = 0;
+    },
+    handleEnter(el, done) {
+      el.style.transition = "opacity 1s";
+      el.style.opacity = 1;
+      done();
+    },
+    handleLeave(el, done) {
+      el.style.transition = "opacity 1s";
+      el.style.opacity = 0;
+      setTimeout(() => {
+        done();
+      }, 1000);
     }
   }
 };
 </script>
 
 <template>
-  <MasonryWall
-    v-if="sortedByPriotity.length"
-    :items="this.sortedByPriotity"
-    :max-columns="3"
-    :column-width="200"
-    :gap="16"
-    :key="masonryKey"
-    @layout="onLayout"
-    style="margin-top: 10vh"
+  <div v-if="isLoading" class="loading"></div>
+  <transition
+    mode="out-in"
+    @before-enter="handleBeforeEnter"
+    @enter="handleEnter"
+    @leave="handleLeave"
   >
-    <template #default="{ item, index }">
-      <Project :project="item" />
-    </template>
-  </MasonryWall>
-  <Footer></Footer>
+    <div v-if="showContent" :key="masonryKey">
+      <MasonryWall
+        v-if="sortedByPriotity.length"
+        :items="sortedByPriotity"
+        :max-columns="3"
+        :column-width="200"
+        :gap="16"
+        @layout="onLayout"
+        style="margin-top: 10vh"
+      >
+        <template #default="{ item, index }">
+          <div :key="item.id" class="fade-in">
+            <Project :project="item" />
+          </div>
+        </template>
+      </MasonryWall>
+      <Footer></Footer>
+    </div>
+  </transition>
 </template>
 
-<style></style>
+<style scoped>
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.fade-in {
+  animation: fadeIn 0.2s ease-in forwards;
+}
+
+.hidden {
+  opacity: 0;
+}
+
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 24px;
+}
+</style>
